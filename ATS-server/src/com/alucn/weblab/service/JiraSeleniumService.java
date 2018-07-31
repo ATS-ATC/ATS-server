8ceb4bd0ea7418b87b44f76e422a65434d573b55
kali_ey <guoyouworld@163.com>
2018-07-31 16:29:37 +0800

    fix shell bug (test)

--- ATS-server/src/com/alucn/weblab/service/JiraSeleniumService.java
+++ ATS-server/src/com/alucn/weblab/service/JiraSeleniumService.java
@@ -3,7 +3,10 @@ package com.alucn.weblab.service;
 import java.io.BufferedReader;
 import java.io.Closeable;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -387,9 +390,9 @@ public class JiraSeleniumService {
 						rps.setString(1, hashMap.get("releases").toString());
 						rps.setString(2, hashMap.get("case_name").toString());
 						rps.addBatch();
-						String cmd="sed -i \"s/\\\"porting_release\\\": \\[.*\\]/\\\"porting_release\\\": \\[\\\""+hashMap.get("releases").toString()+"\\\"\\]/g\" /home/surepayftp/DftCase/"+hashMap.get("case_name").toString();
+						String[] cmd={"/bin/sh","-c","sed -i \"s/\\\"porting_release\\\": \\[.*\\]/\\\"porting_release\\\": \\[\\\""+hashMap.get("releases").toString()+"\\\"\\]/g\" /home/surepayftp/DftCase/"+hashMap.get("case_name").toString()};
 						logger.info(cmd);
-						String result = execCmd(cmd, null);
+						String result = exeCmd(cmd);
 				        System.out.println(result);
 					}
 	                //其余的判断是否有过更新，有-回滚-记录，无-记录
@@ -470,61 +473,28 @@ public class JiraSeleniumService {
 			logger.error("jira_id_mid is null,can not update");
 		}
 	}
-	/**
-     * 执行系统命令, 返回执行结果
-     *
-     * @param cmd 需要执行的命令
-     * @param dir 执行命令的子进程的工作目录, null 表示和当前主进程工作目录相同
-     */
-    public static String execCmd(String cmd, File dir) throws Exception {
-        StringBuilder result = new StringBuilder();
-
-        Process process = null;
-        BufferedReader bufrIn = null;
-        BufferedReader bufrError = null;
-
+	public static String exeCmd(String[] command) throws InterruptedException {
+        String returnString = "";
+        Process pro = null;
+        Runtime runTime = Runtime.getRuntime();
+        if (runTime == null) {
+            System.err.println("Create runtime false!");
+        }
         try {
-            // 执行命令, 返回一个子进程对象（命令在子进程中执行）
-            process = Runtime.getRuntime().exec(cmd, null, dir);
-
-            // 方法阻塞, 等待命令执行完成（成功会返回0）
-            process.waitFor();
-
-            // 获取命令执行结果, 有两个结果: 正常的输出 和 错误的输出（PS: 子进程的输出就是主进程的输入）
-            bufrIn = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
-            bufrError = new BufferedReader(new InputStreamReader(process.getErrorStream(), "UTF-8"));
-
-            // 读取输出
-            String line = null;
-            while ((line = bufrIn.readLine()) != null) {
-                result.append(line).append('\n');
+            pro = runTime.exec(command);
+            BufferedReader input = new BufferedReader(new InputStreamReader(pro.getInputStream()));
+            PrintWriter output = new PrintWriter(new OutputStreamWriter(pro.getOutputStream()));
+            String line;
+            while ((line = input.readLine()) != null) {
+                returnString = returnString + line + "\n";
             }
-            while ((line = bufrError.readLine()) != null) {
-                result.append(line).append('\n');
-            }
-
-        } finally {
-            closeStream(bufrIn);
-            closeStream(bufrError);
-
-            // 销毁子进程
-            if (process != null) {
-                process.destroy();
-            }
+            input.close();
+            output.close();
+            pro.destroy();
+        } catch (IOException ex) {
+        	ex.printStackTrace();
         }
-
-        // 返回执行结果
-        return result.toString();
-    }
-
-    private static void closeStream(Closeable stream) {
-        if (stream != null) {
-            try {
-                stream.close();
-            } catch (Exception e) {
-                // nothing
-            }
-        }
+        return returnString;
     }
     public static void main(String[] args) {
     	try {
